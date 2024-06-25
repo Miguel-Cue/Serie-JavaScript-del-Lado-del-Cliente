a084fd74cde80d31858b7334210e49556432eb69
Miguel-Cue <ayalacuevas53@gmail.com>
2024-06-25 11:01:48 -0400

    Update validation.js

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -1,33 +1,25 @@
 function emailValidation() {
-    const form = document.getElementById('form');
-    form.addEventListener('submit', e => {
-      e.preventDefault();
+  const form = document.getElementById('form');
+  const emailComfirmField = document.getElementById('email_confirm');
+  const element = document.createElement('p');
+  const message = document.createTextNode("Eメールが一致しません");
+  const contentField = document.getElementById('content_field');
+    element.appendChild(message);
+    element.setAttribute('id', 'alert');
+    element.classList.add("alert_color");
+    emailComfirmField.addEventListener('input', e => {
       if(form.email.value !== form.email_confirm.value) {
-        const element = document.createElement('p')
-        const message = document.createTextNode("Eメールが一致しません")
-        form.appendChild(element);
-        element.appendChild(message);
-        element.classList.add("alert");
-        setTimeout(() => {
-          form.removeChild(element)
-        }, 3000)
-      } else {
-        form.submit();
-      }
-    });
-  };
-  
+        if (!document.getElementById('alert')) {
+          contentField.parentNode.insertBefore(element, contentField);
+          emailComfirmField.classList.add("alert_bg");
+        }
+    } else {
+      emailComfirmField.classList.remove("alert_bg");
+      element.parentNode.removeChild(element);
+    }
+});
+};
+
   window.onload = function() {
     emailValidation();
   };
-  /*function confirmSubmit() {
-    const forms = document.forms;
-    forms[0].onsubmit = function(){
-      if (!(confirm("¿Estás seguro de que quieres enviarlo?"))) {
-        alert("fue cancelado");
-        return false;
-      }
-    };
-  };
-  
-  window.onload = confirmSubmit;*/
